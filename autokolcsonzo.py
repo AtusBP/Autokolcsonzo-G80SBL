66f175eb4f005170a29e4618f07a11c250aa3851
Attila Lindvurm <lindvurm@gmail.com>
2026-05-18 12:09:20 +0100

    Day 7 – Kommentek hozzáadva

--- autokolcsonzo.py
+++ autokolcsonzo.py
@@ -54,6 +54,23 @@ class Autokolcsonzo:
 
     def elore_feltoltes(self):
         """Példaadatok betöltése a program indulásakor."""
-        self.auto_hozzaadas(Szemelyauto("ABC-123", "Opel Astra", 12000, 5))
-        self.auto_hozzaadas(Szemelyauto("XYZ-555", "Suzuki Swift", 10000, 3))
-        self.auto_hozzaadas(Teherauto("TTT-999", "Ford Transit", 18000, 1200))
+
+        # --- Autók ---
+        auto1 = Szemelyauto("ABC-123", "Opel Astra", 12000, 5)
+        auto2 = Szemelyauto("XYZ-555", "Suzuki Swift", 10000, 3)
+        auto3 = Teherauto("TTT-999", "Ford Transit", 18000, 1200)
+
+        self.auto_hozzaadas(auto1)
+        self.auto_hozzaadas(auto2)
+        self.auto_hozzaadas(auto3)
+
+        # --- Előre definiált bérlések ---
+        berles1 = Berles(auto1, "2026-05-01", "Kiss Péter")
+        berles2 = Berles(auto2, "2026-05-02", "Nagy Anna")
+        berles3 = Berles(auto3, "2026-05-03", "Szabó Márton")
+        berles4 = Berles(auto1, "2026-05-04", "Tóth Lilla")
+
+        self._berlesek.append(berles1)
+        self._berlesek.append(berles2)
+        self._berlesek.append(berles3)
+        self._berlesek.append(berles4)
